fb47f0f6faeebdfcb20fb9747875b31bde731452
acstanie <alexhartman95@gmail.com>
2023-11-22 12:27:50 -0400

    Completed execise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+let string = "";
+const num = Math.floor(Math.random() * 1000);
 
+const repeatString = function(string, num) {
+    let result = "";
+
+    if(num < 0) {
+        result += "ERROR";
+    } else {
+        for(let i = 0; i < num; i++) {
+            result += string;
+        }
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
