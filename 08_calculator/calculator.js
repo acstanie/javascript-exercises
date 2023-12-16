a5a2b624c8c1183c42c1bc9ac39a59ebe2835fe2
acstanie <alexhartman95@gmail.com>
2023-12-16 12:15:30 -0400

    Completed Exercise 8s

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2; 
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+
+  for(let number of array) {
+    sum += number;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let product = 1;
 
+  for(let number of numbers) {
+    product *= number;
+  }
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let product = 1;
+	for(let i = number; i > 0; i--) {
+    product *= i; 
+  } 
+
+  return product;
 };
 
 // Do not edit below this line
